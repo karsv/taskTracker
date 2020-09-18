ac79ba07e1cf385ecd093f4ac9afa81c639dbbb6
karsv <Pbvf2019!>
2020-09-18 22:20:26 +0300

    add models

---
+++ src/main/java/com/example/taskTracker/model/Task.java
@@ -0,0 +1,96 @@
+package com.example.taskTracker.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Long idOfCreator;
+    private String title;
+    private String description;
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+    private LocalDate dateOfCreated;
+    private LocalDate dateOfStatusChange;
+
+    public Task() {
+    }
+
+    public LocalDate getDateOfStatusChange() {
+        return dateOfStatusChange;
+    }
+
+    public void setDateOfStatusChange(LocalDate dateOfStatusChange) {
+        this.dateOfStatusChange = dateOfStatusChange;
+    }
+
+    public Long getIdOfCreator() {
+        return idOfCreator;
+    }
+
+    public void setIdOfCreator(Long idOfCreator) {
+        this.idOfCreator = idOfCreator;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public LocalDate getDateOfCreated() {
+        return dateOfCreated;
+    }
+
+    public void setDateOfCreated(LocalDate dateOfCreated) {
+        this.dateOfCreated = dateOfCreated;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(id, task.id) &&
+                Objects.equals(title, task.title) &&
+                Objects.equals(description, task.description) &&
+                status == task.status &&
+                Objects.equals(dateOfCreated, task.dateOfCreated);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description, status, dateOfCreated);
+    }
+}
