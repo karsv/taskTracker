cd210f34587f8ef6ca47b2623abc6576adf831d8
karsv <Pbvf2019!>
2020-09-21 16:37:55 +0300

    add operations with task to user service

--- src/main/java/com/example/taskTracker/service/TaskService.java
+++ src/main/java/com/example/taskTracker/service/TaskService.java
@@ -2,11 +2,12 @@ package com.example.taskTracker.service;
 
 import com.example.taskTracker.dto.TaskDto;
 import com.example.taskTracker.model.Task;
+import com.example.taskTracker.model.TaskStatus;
 import java.util.List;
 import org.springframework.data.domain.Page;
 
 public interface TaskService {
-    Task create(TaskDto taskCreateDto);
+    Task create(Long userId, TaskDto taskDto);
 
     Task getById(Long id);
 
@@ -16,7 +17,9 @@ public interface TaskService {
 
     Page<Task> getAllByOldestUsers(Integer page, Integer positionsOnPage);
 
-    Task update(TaskDto taskUpdateDto);
+    Task update(Long taskId, Long userId, TaskDto taskDto);
+
+    Task update(Long taskId, TaskStatus taskStatus);
 
     void delete(Long id);
 }
--- src/main/java/com/example/taskTracker/service/impl/TaskServiceImpl.java
+++ src/main/java/com/example/taskTracker/service/impl/TaskServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.taskTracker.service.impl;
 
 import com.example.taskTracker.dao.TaskEntityRepository;
 import com.example.taskTracker.dto.TaskDto;
+import com.example.taskTracker.dto.UserResponseDto;
 import com.example.taskTracker.exceptions.TaskServiceException;
 import com.example.taskTracker.model.Task;
 import com.example.taskTracker.model.TaskStatus;
@@ -13,8 +14,10 @@ import org.mapstruct.factory.Mappers;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Service
 public class TaskServiceImpl implements TaskService {
     private final TaskEntityRepository taskEntityRepository;
     private final UserService userService;
@@ -26,10 +29,16 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     @Transactional
-    public Task create(TaskDto taskCreateDto) {
-        Task task = Mappers.getMapper(TaskMapper.class).taskDtoToTask(taskCreateDto);
-        userService.addTask(taskCreateDto.getUserId(), task);
-        return taskEntityRepository.save(task);
+    public Task create(Long userId, TaskDto taskDto) {
+        try {
+            Task task = Mappers.getMapper(TaskMapper.class).taskDtoToTask(taskDto);
+            task.setIdOfCreator(userId);
+            Task savedTask = taskEntityRepository.save(task);
+            userService.addTask(userId, savedTask);
+            return savedTask;
+        } catch (IllegalArgumentException e){
+            throw new TaskServiceException("No such status for task!");
+        }
     }
 
     @Override
@@ -61,8 +70,9 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     @Transactional
-    public Task update(TaskDto taskDto) {
-        Task task = getById(taskDto.getUserId());
+    public Task update(Long taskId, Long userId, TaskDto taskDto) {
+        Task task = getById(taskId);
+        task.setIdOfCreator(userId);
         task.setStatus(TaskStatus.valueOf(taskDto.getStatus()));
         task.setTitle(taskDto.getTitle());
         task.setDescription(taskDto.getDescription());
@@ -70,7 +80,18 @@ public class TaskServiceImpl implements TaskService {
     }
 
     @Override
+    public Task update(Long taskId, TaskStatus taskStatus) {
+        Task task = getById(taskId);
+        task.setStatus(taskStatus);
+        return taskEntityRepository.save(task);
+    }
+
+    @Override
+    @Transactional
     public void delete(Long id) {
+        Task task = taskEntityRepository.findById(id)
+                .orElseThrow(() -> new TaskServiceException("No task with such id!"));
+        userService.updateTasks(task.getIdOfCreator(), task);
         taskEntityRepository.deleteById(id);
     }
 }
