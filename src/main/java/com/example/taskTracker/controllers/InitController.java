5605c6b922adee36f9a5ab8d7b2bb43a1fc5db62
karsv <Pbvf2019!>
2020-09-21 16:39:24 +0300

    implement init for 100 users and 1000 tasks

--- src/main/java/com/example/taskTracker/controllers/InitController.java
+++ src/main/java/com/example/taskTracker/controllers/InitController.java
@@ -1,30 +1,50 @@
 package com.example.taskTracker.controllers;
 
-import com.example.taskTracker.dto.UserDto;
+import com.example.taskTracker.dao.UserEntityRepository;
+import com.example.taskTracker.dto.TaskDto;
+import com.example.taskTracker.model.TaskStatus;
 import com.example.taskTracker.model.User;
+import com.example.taskTracker.service.TaskService;
 import com.example.taskTracker.service.UserService;
 import javax.annotation.PostConstruct;
+import java.time.LocalDate;
+import java.util.Random;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 
 @Component
 public class InitController {
     private final UserService userService;
+    private final TaskService taskService;
+    private final UserEntityRepository userEntityRepository;
 
-    public InitController(UserService userService) {
+    public InitController(UserService userService,
+                          TaskService taskService,
+                          UserEntityRepository userEntityRepository) {
         this.userService = userService;
+        this.taskService = taskService;
+        this.userEntityRepository = userEntityRepository;
     }
 
     @PostConstruct
-    private void init(){
+    private void init() {
+        Random random = new Random();
         for (int i = 0; i < 100; i++) {
-            UserDto user = new UserDto();
-            user.setEmail("userNo"+ i+"test.com");
+            User user = new User();
+            user.setEmail("userNo" + i + "@test.com");
             user.setFirstName("fn" + i);
             user.setLastName("ln" + i);
             user.setPassword("test");
-            user.setRepeatedPassword("test");
-            userService.create(user);
+            user.setDateOfRegister(LocalDate.of(2020, i / 10 + 1, 1 + i/28));
+            userEntityRepository.save(user);
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            TaskDto taskDto = new TaskDto();
+            taskDto.setStatus(TaskStatus.values()[random.nextInt(TaskStatus.values().length)]
+                    .toString());
+            taskDto.setDescription("description " + i);
+            taskDto.setTitle("title " + i);
+            taskService.create((long) random.nextInt(99) + 1 ,taskDto);
         }
     }
 }
