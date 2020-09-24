398a2a8a45c691399f382095bca8cd1b7231fa52
karsv <Pbvf2019!>
2020-09-24 13:45:53 +0300

    update ReadMe.md try to configure Swagger

--- src/main/java/com/example/taskTracker/config/SwaggerConfigure.java
+++ src/main/java/com/example/taskTracker/config/SwaggerConfigure.java
@@ -1,12 +1,15 @@
 package com.example.taskTracker.config;
 
-import java.util.Arrays;
+import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -15,15 +18,33 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfigure {
     @Bean
     public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.any())
+//                .paths(PathSelectors.any())
+//                .build()
+//                .securitySchemes(Arrays.asList(apiKey()));
         return new Docket(DocumentationType.SWAGGER_2)
+                .securitySchemes(Collections.singletonList(new ApiKey("JWT", "Authorization", "header")))
+                .securityContexts(Collections.singletonList(
+                        SecurityContext.builder()
+                                .securityReferences(
+                                        Collections.singletonList(SecurityReference.builder()
+                                                .reference("JWT")
+                                                .scopes(new AuthorizationScope[0])
+                                                .build()
+                                        )
+                                )
+                                .build())
+                )
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build()
-                .securitySchemes(Arrays.asList(apiKey()));
+                .apis(RequestHandlerSelectors
+                        .basePackage("com.example.testTracker"))
+                .paths(PathSelectors.regex("/.*"))
+                .build();
     }
 
     private ApiKey apiKey() {
-        return new ApiKey("Authorization", "api_key", "header");
+        return new ApiKey("apiKey", "Authorization", "header");
     }
 }
