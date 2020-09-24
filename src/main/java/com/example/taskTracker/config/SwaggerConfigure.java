9ecedbe2ccb9f35044d35d75c70187cb8d2ec80f
karsv <Pbvf2019!>
2020-09-24 12:07:00 +0300

    tries to configure swagger

--- src/main/java/com/example/taskTracker/config/SwaggerConfigure.java
+++ src/main/java/com/example/taskTracker/config/SwaggerConfigure.java
@@ -1,9 +1,11 @@
 package com.example.taskTracker.config;
 
+import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,6 +19,11 @@ public class SwaggerConfigure {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .securitySchemes(Arrays.asList(apiKey()));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "api_key", "header");
     }
 }
